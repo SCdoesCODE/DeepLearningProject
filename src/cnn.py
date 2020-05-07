47fe13d973f62b3474165747b0eeb8d44a36f6ab
emilelmarsson <emil.elmarsson@gmail.com>
2020-05-07 15:50:23 +0200

    Added some tensorflow stuff

--- src/cnn.py
+++ src/cnn.py
@@ -12,6 +12,21 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 import pickle
 
-X = np.loadtxt('flattened_images.txt')
+def decode_img(img):
+    # convert compressed string to a uint8 tensor
+    img = tf.image.decode_png(img, channels=1)
+    # convert to floats in range [0,1]
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # resize image
+    return tf.image.resize(img, [256, 256])
 
-X = X/255.0
+def process_path(file_path):
+    label = get_label(file_path)
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    return img, label
+
+image_paths = tf.data.Dataset.list_files("images_*/images/*")
+
+for image_path in image_paths.take(10):
+    print(image_path.numpy())
