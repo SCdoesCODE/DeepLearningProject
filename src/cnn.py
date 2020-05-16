3fea803d65fb74f55afcd9f17930c179bb90488c
Emil Elmarsson <emil.elmarsson@gmail.com>
2020-05-16 12:10:28 +0000

    AUC and class weights

--- src/cnn.py
+++ src/cnn.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from numpy.random import default_rng
 #from google.cloud import storage
@@ -11,12 +12,15 @@ import cv2
 import pandas
 import pickle
 import tensorflow as tf
+from sklearn.utils.class_weight import compute_sample_weight, compute_class_weight 
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model, Model
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling2D
+from tensorflow.keras.callbacks import Callback, EarlyStopping, ModelCheckpoint
+from tensorflow.keras.applications import ResNet50, VGG16
 import logging
 
 # For suppressing annoying log messages
@@ -42,6 +46,8 @@ HOME = expanduser("~")
 classes=[]
 # CSV object
 names_and_labels=[]
+# Class weights
+class_weights = []
 
 # Loading all labels from the CSV file
 def init():
@@ -61,7 +67,6 @@ init()
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-LR = 0.0001
 BATCH_SIZE = 32
 NUM_EPOCHS = 30
 NUM_CLASSES = len(classes)
@@ -72,10 +77,11 @@ TEST_FRAC = 0.2
 SHUFFLE_BUFFER_SIZE = 1024
 IMG_HEIGHT = 256
 IMG_WIDTH = 256
+CHANNELS = 3
 
 def decode_img(img):
     # convert compressed string to a uint8 tensor
-    img = tf.image.decode_png(img, channels=1)
+    img = tf.image.decode_png(img, channels=CHANNELS)
     # convert color values to floats in range [0,1]
     img = tf.image.convert_image_dtype(img, tf.float32)
     # resize image
@@ -94,20 +100,20 @@ def prepare_dataset(dataset, training=True):
     dataset = dataset.prefetch(buffer_size=AUTOTUNE)
     return dataset
 
-# Takes an array of label names and returns the one-hot encoding label
-def one_hot_encoding(label_names):
-    one_hot = np.zeros(NUM_CLASSES)
+# Takes an array of label names and returns the multi-hot encoding label
+def multi_hot_encoding(label_names):
+    multi_hot = np.zeros(NUM_CLASSES)
     for label_name in label_names:
         index = classes.index(label_name)
-        one_hot[index] = 1
-    return one_hot
+        multi_hot[index] = 1
+    return multi_hot
 
-# One-hot encodes all labels using helper function above
-def one_hot_encode_labels(labels):
+# Multi-hot encodes all labels using helper function above
+def multi_hot_encode_labels(labels):
     new_labels = np.zeros((len(labels), NUM_CLASSES))
     for i,label in enumerate(labels):
-        new_labels[i] = one_hot_encoding(label)
-    return new_labels[:,1:]
+        new_labels[i] = multi_hot_encoding(label)
+    return new_labels
 
 # For saving plots
 def save_plot(history):
@@ -156,10 +162,17 @@ def create_data():
     val_images = tf.data.Dataset.from_tensor_slices(val_paths).map(process_path, num_parallel_calls=AUTOTUNE)
     test_images = tf.data.Dataset.from_tensor_slices(test_paths).map(process_path, num_parallel_calls=AUTOTUNE)
 
+    # Calculating class weights
+    global class_weights
+    train_labels = multi_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(train_names)]['Finding Labels'].to_numpy())
+    class_weights = np.sum(train_labels) / np.sum(train_labels, axis=0)
+    print(np.sum(train_labels, axis=0))
+    print(class_weights)
+
     # Mapping the filenames to labels
-    train_labels = tf.data.Dataset.from_tensor_slices(one_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(train_names)]['Finding Labels'].to_numpy()))
-    val_labels = tf.data.Dataset.from_tensor_slices(one_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(val_names)]['Finding Labels'].to_numpy()))
-    test_labels = tf.data.Dataset.from_tensor_slices(one_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(test_names)]['Finding Labels'].to_numpy()))
+    train_labels = tf.data.Dataset.from_tensor_slices(train_labels)
+    val_labels = tf.data.Dataset.from_tensor_slices(multi_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(val_names)]['Finding Labels'].to_numpy()))
+    test_labels = tf.data.Dataset.from_tensor_slices(multi_hot_encode_labels(names_and_labels[names_and_labels['Image Index'].isin(test_names)]['Finding Labels'].to_numpy()))
 
     # Preparing data for training
     train_ds = prepare_dataset(tf.data.Dataset.zip((train_images, train_labels)))
@@ -168,22 +181,51 @@ def create_data():
 
     return train_ds, val_ds, test_ds
 
-def create_model():
-    model = Sequential()
-    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,1)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
+METRICS = [
+    tf.keras.metrics.TruePositives(name='tp'),
+    tf.keras.metrics.FalsePositives(name='fp'),
+    tf.keras.metrics.TrueNegatives(name='tn'),
+    tf.keras.metrics.FalseNegatives(name='fn'), 
+    tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.AUC(name='auc'),
+    tf.keras.metrics.Recall(name='recall'),
+]
 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='sigmoid'))
-    model.add(layers.Dense(NUM_CLASSES-1))
+def create_model(metrics=METRICS, output_bias=None):
+    if output_bias is not None:
+        output_bias = tf.keras.initializers.Constant(output_bias)
+    
+    '''
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,1)))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3,3), activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
+    
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(NUM_CLASSES, activation='sigmoid'))
+    '''
+
+    base_model = ResNet50(include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, CHANNELS))
+
+    model = base_model.output
+    model = GlobalAveragePooling2D()(model)
+    model = Dense(64, activation='relu')(model)
+    predictions = Dense(NUM_CLASSES, activation='sigmoid')(model)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model.compile(optimizer='adam',
+              loss="binary_crossentropy",
+              metrics=metrics)
+
     return model
 
 train_ds, val_ds, test_ds = create_data()
@@ -191,27 +233,24 @@ train_ds, val_ds, test_ds = create_data()
 model = create_model()
 model.summary()
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
-              loss="binary_crossentropy",
-              metrics=['accuracy'])
-
 # Saving the best model every epoch (based on val loss)
 checkpoint_path = HOME + "/DeepLearningProject/models/nih_model.h5"
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
-                                                 save_best_only=True)
+cp_callback = ModelCheckpoint(filepath=checkpoint_path,
+                              save_best_only=True)
 
-# Early stopping based on validation loss
-es_callback = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3)
+# Early stopping based on AUC
+es_callback = EarlyStopping(monitor="val_auc", patience=5, mode='max', restore_best_weights=True)
 
 # Fitting
 history = model.fit(train_ds, 
                     validation_data=val_ds,
                     epochs=NUM_EPOCHS,
-                    callbacks=[cp_callback, es_callback])
+                    callbacks=[cp_callback, es_callback],
+                    class_weight=class_weights)
 
 # Evaluating model
 test_ds = prepare_dataset(test_ds, training=False)
 model.evaluate(test_ds, verbose=1)
 
 # Saving plot
-save_plot(history)
+#save_plot(history)
